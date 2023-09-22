b831c37d3ad9ec4f96aa32c6fefcff83718a504f
RaikkoP <RaikkoP@users.noreply.github.com>
2023-09-23 01:13:50 +0300

    Added module exports

--- controllers/article.js
+++ controllers/article.js
@@ -1,124 +1,115 @@
 const Article = require("../models/article.model");
 
-const getArticleBySlug = (req, res) => {
-    Article.getBySlug(req.params.slug, (err, data) => {
+module.exports = {
+  getAllArticles: async (req, res) => {
+    if (req.session.authenticated) {
+      Article.getAll((err, data) => {
         if (err) {
-            res.status(500).send({
-                message: err.message || "Some error occured retrieving articles"
-            });
+          res.status(500).send({
+            message:
+              err.message || "Some error occured retrieving articles data",
+          });
         } else {
-            res.render("article", {
-                article: data
-            })
+          res.render("index", {
+            articles: data,
+          });
         }
-    });
-};
+      });
+    } else {
+      res.redirect("/login");
+    }
+  },
 
-const createNewArticle = (req, res) => {
-    console.log('new article');
+  getArticleBySlug: async (req, res) => {
+    Article.getBySlug(req.params.slug, (err, data) => {
+      if (err) {
+        res.status(500).send({
+          message: err.message || "Some error occured retrieving articles",
+        });
+      } else {
+        res.render("article", {
+          article: data,
+        });
+      }
+    });
+  },
+
+  createNewArticle: async (req, res) => {
+    console.log("new article");
 
     const newArticle = new Article({
-        name: req.body.name,
-        slug: req.body.slug,
-        image: req.body.image,
-        body: req.body.body,
-        published: new Date().toISOString().slice(0, 19).replace('T', ' '),
-        author_id: req.body.author_id
-    }
-    )
-     
+      name: req.body.name,
+      slug: req.body.slug,
+      image: req.body.image,
+      body: req.body.body,
+      published: new Date().toISOString().slice(0, 19).replace("T", " "),
+      author_id: req.body.author_id,
+    });
+
     Article.createNew(newArticle, (err, data) => {
-        if (err) {
-            res.status(500).send({
-                message: err.message || "Some error occured sending article data"
-            })
-        } else {
-            res.redirect('/')
-        }
-    })
-};
+      if (err) {
+        res.status(500).send({
+          message: err.message || "Some error occured sending article data",
+        });
+      } else {
+        res.redirect("/");
+      }
+    });
+  },
 
-const updateArticle = (req, res) => {
-
+  updateArticle: async (req, res) => {
     const newValues = new Article({
-        id: req.body.id,
-        name: req.body.name,
-        published: req.body.published,
-        slug: req.body.slug,
-        image:req.body.image,
-        body: req.body.body,
-        author_id: req.body.author_id
+      id: req.body.id,
+      name: req.body.name,
+      published: req.body.published,
+      slug: req.body.slug,
+      image: req.body.image,
+      body: req.body.body,
+      author_id: req.body.author_id,
     });
 
     Article.updatePost(newValues, (err, data) => {
-        if (err) {
-            res.status(500).send({
-                message: err.message || "Some error occured updating"
-            })
-        } else {
-            res.redirect('/');
-        }
+      if (err) {
+        res.status(500).send({
+          message: err.message || "Some error occured updating",
+        });
+      } else {
+        res.redirect("/");
+      }
     });
-};
+  },
 
-const deleteArticle = (req, res) => {
-    
-
+  deleteArticle: async (req, res) => {
     Article.deletePost(req.params.id, (err, data) => {
-        if(err) {
-            res.status(500).sind({
-                message: err.messasge || "Failed to delete post"
-            })
-        } else {
-            res.redirect('/');
-        }
-    })
-}
+      if (err) {
+        res.status(500).sind({
+          message: err.messasge || "Failed to delete post",
+        });
+      } else {
+        res.redirect("/");
+      }
+    });
+  },
 
-const articleForm = (req, res) => {
-    res.render("form")
-};
+  articleForm: (req, res) => {
+    res.render("form");
+  },
 
-const getArticleByID = (req, res) => {
-    console.log('update article');
+  getArticleByID: async (req, res) => {
+    console.log("update article");
 
     Article.getPost(req.params.id, (err, data) => {
-        if(err){
-            res.status(500).send({
-                message: err.message || "Didn't find post"
-            });
-        } else if (data.length > 0) {
-            res.render("editForm", {
-                article: data
-            })
-        } else {
-            res.redirect("/");
-        }
+      if (err) {
+        res.status(500).send({
+          message: err.message || "Didn't find post",
+        });
+      } else if (data.length > 0) {
+        res.render("editForm", {
+          article: data,
+        });
+      } else {
+        res.redirect("/");
+      }
     });
-}
-
-module.exports = {
-    getAllArticles: async (req, res) => {
-        if(req.session.authenticated){
-            Article.getAll((err, data) => {
-                if (err) {
-                    res.status(500).send({
-                        message: err.message || "Some error occured retrieving articles data"
-                    })
-                } else {
-                    res.render("index", {
-                        articles: data
-                    });
-                }
-            })
-        } else {
-            res.redirect('/login')
-        }
-    },
-    getArticleBySlug,
-    createNewArticle,
-    articleForm,
-    getArticleByID,
-    updateArticle,
-    deleteArticle
+  },
 };
